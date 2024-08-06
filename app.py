eaaafb8a3ef1de54daa2c7cafee2060ac78e7224
matheus770 <matquill@gmail.com>
2024-08-05 23:49:29 -0300

    Alterando o banco de dados, tratativa de duplicidade de habitos no banco, atualização do html e css

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import random
 from flask import Flask, render_template, request, redirect, url_for
 from config import Config
-from models import Habit, db
+from models import CategoryHabit, Habit, HabitCategories, HabitEntries, HabitEntry, db
 from utils import *
 from sqlalchemy import text
 
@@ -24,23 +24,64 @@ def index():
     data_info_dict = obtem_dias_mes_atual()
     qtd_dias_mes = data_info_dict['dias_mes']
     nome_mes = data_info_dict['nome_mes']
-    levels = [random.randint(0, 4) for _ in range(qtd_dias_mes)]
+    ano_atual = data_info_dict['ano_atual']
+    mes_atual = data_info_dict['mes_atual']
     
-    habitos = Habit.query.all()
-    return render_template('habits.html',habitos=habitos,num_dias_mes=qtd_dias_mes, levels=levels,nome_mes=nome_mes)
+    habitos = Habit.query.all() 
+    
+    habito_levels = {}
+    
+    for habito in habitos:
+        levels = []
+        for dia in range(1, qtd_dias_mes + 1):
+            date = datetime(ano_atual, mes_atual, dia).date()
+            entry = HabitEntry.query.join(HabitEntries).filter(
+                HabitEntries.id_habit == habito.id,
+                HabitEntry.date == date
+            ).first()
+            if entry and entry.status:
+                levels.append(1)  
+            else:
+                levels.append(0) 
+        habito_levels[habito.id] = levels
+    
+    return render_template('habits.html', habitos=habitos, num_dias_mes=qtd_dias_mes, habito_levels=habito_levels, nome_mes=nome_mes)
     
 @app.route('/add_habit', methods=['POST'])
 def add_habit():
     name = request.form.get('name')
     category = request.form.get('category')
+    try:
+        #* Verifica se já existe uma categoria com o mesmo nome
+        existing_category = CategoryHabit.query.filter_by(name=category).first()
+        
+        if not existing_category:
+            #* Se não existir, cria uma nova categoria
+            new_category = CategoryHabit(name=category)
+            db.session.add(new_category)
+            db.session.commit()  # Commit para salvar a nova categoria
 
-    new_habit = Habit(name=name, category=category)
-    db.session.add(new_habit)
-    db.session.commit()
+        #* Obtém a categoria existente ou recém-criada
+        category = existing_category if existing_category else new_category
+        
+        #* Verifica se já existe um hábito com o mesmo nome
+        existing_habit = Habit.query.filter_by(name=name).first()
+        
+        if not existing_habit:
+            #* Se não existir, cria um novo hábito
+            new_habit = Habit(name=name)
+            db.session.add(new_habit)
+            db.session.commit()  #* Commit para salvar o novo hábito
 
-    return redirect(url_for('index'))
-    
-    
+            #* Associa o hábito à categoria
+            new_habit.categories.append(category)
+            db.session.commit()  #* Commit para salvar a associação
+
+        return redirect(url_for('index'))
+    except Exception as e:
+        print(f"Erro: {e}")
+        return redirect(url_for('index'))
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -2,17 +2,34 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Habit(db.Model):
-    __tablename__ = 'habits'
+class CategoryHabit(db.Model):
+    __tablename__ = 'categories_habits'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    category = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False, index=True)
     description = db.Column(db.Text, nullable=True)
-    entries = db.relationship('HabitEntry', backref='habit', lazy=True)
+    habits = db.relationship('Habit', secondary='habits_categories', backref=db.backref('categories', lazy='dynamic'))
 
 class HabitEntry(db.Model):
     __tablename__ = 'habit_entries'
     id = db.Column(db.Integer, primary_key=True)
-    habit_id = db.Column(db.Integer, db.ForeignKey('habits.id'), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    status = db.Column(db.Boolean, default=False, nullable=False)
+    date = db.Column(db.Date, nullable=False, index=True)
+    status = db.Column(db.Boolean, default=False, nullable=False, index=True)
+    habits = db.relationship('Habit', secondary='habits_entries', backref=db.backref('entries', lazy='dynamic'))
+
+class Habit(db.Model):
+    __tablename__ = 'habits'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False, index=True)
+    description = db.Column(db.Text, nullable=True)
+
+class HabitCategories(db.Model):
+    __tablename__ = 'habits_categories'
+    id_habit = db.Column(db.Integer, db.ForeignKey('habits.id'), primary_key=True)
+    id_category = db.Column(db.Integer, db.ForeignKey('categories_habits.id'), primary_key=True)
+    db.Index('idx_habit_category', 'id_habit', 'id_category')
+
+class HabitEntries(db.Model):
+    __tablename__ = 'habits_entries'
+    id_habit = db.Column(db.Integer, db.ForeignKey('habits.id'), primary_key=True)
+    id_entry = db.Column(db.Integer, db.ForeignKey('habit_entries.id'), primary_key=True)
+    db.Index('idx_habit_entry', 'id_habit', 'id_entry')
